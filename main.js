ab5cff526dd7013809e933401afebc544adaed95
XVII <karshibaevnurmuhammad@gmail.com>
2024-01-08 10:35:00 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var password = "Oybek";
+
+function passcheck() {
+    if (document.getElementById("pass1").value != password) {
+        alert("Parol Oybek");
+        return false;
+    }
+
+    if (document.getElementById("pass1").value == password) {
+    }
+}
